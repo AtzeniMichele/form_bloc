08d498a72194f73a991b9d961689918628019a35
AtzeniMichele <michele.atzeni@studenti.unipd.it>
2022-10-06 20:26:17 +0200

    scroll

--- lib/src/stepper/stepper.dart
+++ lib/src/stepper/stepper.dart
@@ -746,7 +746,6 @@ class _StepperState extends State<Stepper> with TickerProviderStateMixin {
         ),
         Expanded(
           child: ListView(
-            shrinkWrap: true,
             physics: widget.physics,
             padding: const EdgeInsets.all(24.0),
             children: <Widget>[
